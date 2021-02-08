bffb7cb8165919733bea7a27dff4f3c668e2c6be
Andrey Safonov <a.so777@live.ca>
2021-02-08 13:12:37 -0800

    Added pdf.js sample

---
+++ pdfjs-sample/my-pdf-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        id="pdf-js-viewer"
+        src="lib/web/viewer.html?file=../../incometaxact.pdf"
+        title="webviewer"
+        frameborder="0"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
